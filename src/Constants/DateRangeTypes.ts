268c8d962035e4936b179a37a582217f2c2107d3
cameronknaus <cdknaus@gmail.com>
2023-10-08 19:22:09 -0400

    Convert date range hook to typescript

--- src/Constants/DateRangeTypes.ts
+++ src/Constants/DateRangeTypes.ts
@@ -1,8 +1,15 @@
+import dayjs from 'dayjs';
+
 enum DateRangeType {
     MONTH = 'MONTH',
     YEAR = 'YEAR',
     MAX = 'MAX',
     SPECIFIC = 'SPECIFIC'
 }
+
+export type DateRange = {
+    startDate: dayjs.Dayjs,
+    endDate: dayjs.Dayjs
+    }
 
 export default DateRangeType;
