073417e2caa032d90ef0c09e8a7b0ba05a0ffec6
cameronknaus <cdknaus@gmail.com>
2024-09-24 15:37:20 -0400

    Add loading prop to transaction row component

--- src/Components/TransactionRow/TransactionRow.tsx
+++ src/Components/TransactionRow/TransactionRow.tsx
@@ -1,6 +1,7 @@
 import { clsx } from 'clsx';
 import Currency from 'Components/Currency/Currency';
 import SpendingCategoryIcon from 'Components/Shared/Icons/SpendingCategoryIcon';
+import SkeletonLoader from 'Components/Shared/SkeletonLoader';
 import useContent from 'Hooks/useContent';
 import { ComponentProps } from 'react';
 import { FaChevronRight } from 'react-icons/fa';
@@ -14,6 +15,7 @@ type TransactionRowPropTypes = {
     amountSpent: number;
     note?: string;
     className?: string;
+    isLoading?: boolean;
     onClick: (transactionId: DiscretionaryTransactionId) => void;
 };
 
@@ -24,9 +26,25 @@ export default function TransactionRow({
     note,
     className,
     onClick,
+    isLoading,
     ...attributes
 }: TransactionRowPropTypes & Omit<ComponentProps<'button'>, 'onClick' | 'className'>) {
     const getCategoryLabel = useContent('SPENDING_CATEGORIES');
+
+    if (isLoading) {
+        return (
+            <button className={clsx(styles.transactionsRow, className)} {...attributes}>
+                <div className={styles.transactionDetails}>
+                    <div className={styles.dataRow}>
+                        <SkeletonLoader style={{ height: 20, width: 110 }} />
+                        <SkeletonLoader style={{ height: 20, width: 50 }} />
+                    </div>
+                    <SkeletonLoader style={{ height: 18, width: 60, marginTop: 4 }} />
+                </div>
+                <FaChevronRight className={styles.chevron} />
+            </button>
+        );
+    }
 
     return (
         <button
