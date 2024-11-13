951f5962e5904503660861bc0595e907de8e82c2
cameronknaus <cdknaus@gmail.com>
2024-11-12 23:37:09 -0500

    Fix interactive data row layout

--- src/Hooks/useSpendingService/useSpendingDetailsService.ts
+++ src/Hooks/useSpendingService/useSpendingDetailsService.ts
@@ -5,8 +5,8 @@ import { SpendingDetailsV1Response } from 'Types/Services/spending.model';
 
 export default function useSpendingDetailsService() {
     // TODO: Implement dynamic date range
-    const startDate = '2024-09-01';
-    const endDate = '2024-09-24';
+    const startDate = '2024-11-01';
+    const endDate = '2024-11-12';
 
     return useQuery<SpendingDetailsV1Response>({
         queryKey: ['spending', startDate, endDate],
