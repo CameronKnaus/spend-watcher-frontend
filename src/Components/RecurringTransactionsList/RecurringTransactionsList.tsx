7cad032232e17aa23743f045b986bbb9f8c9799e
cameronknaus <cdknaus@gmail.com>
2025-05-31 11:51:52 -0400

    Fix recurring month bug when on 31st day of month

--- src/Components/RecurringTransactionsList/RecurringTransactionsList.tsx
+++ src/Components/RecurringTransactionsList/RecurringTransactionsList.tsx
@@ -2,7 +2,7 @@ import BottomSheet from 'Components/BottomSheet/BottomSheet';
 import CustomButton from 'Components/CustomButton/CustomButton';
 import EditableRecurringTransactionRow from 'Components/RecurringTransactionRow';
 import AddRecurringTransactionRow from 'Components/RecurringTransactionRow/AddRecurringTransactionRow';
-import { format, parse } from 'date-fns';
+import { format, parse, subMonths } from 'date-fns';
 import useContent from 'Hooks/useContent';
 import useRecurringTransactionsList from 'Hooks/useRecurringTransactionsList/useRecurringTransactionsList';
 import { MonthYearDbDate, monthYearDbDateFormat } from 'Types/dateTypes';
@@ -31,7 +31,7 @@ export default function RecurringTransactionsList({
 
     const oldestTransactionDate = recurringTransactionsList[recurringTransactionsList.length - 1].date;
     // Starting with the current date, we will iterate backwards until we reach the oldest transaction date
-    const currentDate = new Date();
+    let currentDate = new Date();
     const applicableMonths: MonthYearDbDate[] = [];
     let lastTransactionDateReached = false;
     while (!lastTransactionDateReached) {
@@ -43,7 +43,7 @@ export default function RecurringTransactionsList({
         }
 
         // Update current date to the previous month for next iteration
-        currentDate.setMonth(currentDate.getMonth() - 1);
+        currentDate = subMonths(currentDate, 1);
     }
 
     return (
