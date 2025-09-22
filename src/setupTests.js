911fc6d063e329d7dcd18497f17fbf8bf7043ae7
cameronknaus <cdknaus@gmail.com>
2025-09-21 21:12:01 -0500

    Move src contents to web folder

--- src/setupTests.js
+++
@@ -1,3 +0,0 @@
-// Create-react-app searches for src/setupTests.ts to run jest-setup.  If this app is ever ejected, this file is no longer necessary
-import '@testing-library/jest-dom';
-import '../jest-setup';
